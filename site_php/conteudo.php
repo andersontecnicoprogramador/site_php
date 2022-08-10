e99473e2ac25936bc6ef6397a75dd6313904fae3
andersontecnicoprogramador <68762932+andersontecnicoprogramador@users.noreply.github.com>
2022-08-10 00:00:54 -0300

    Add files via upload

---
+++ site_php/conteudo.php
@@ -0,0 +1,19 @@
+<div id="conteudo_principal">
+	<div class="postagens">
+		<h1 class="titulos">Titulo da Postagem</h1>
+		<img src="imagens/postagem.png" class="imagem">
+		<p class="paragrafo">Parágrafo provisorio...</p>
+		<span class="data">09/08/2022</span>
+	</div>
+	<div class="postagens">
+		<h1 class="titulos">Titulo da Postagem</h1>
+		<img src="imagens/postagem.png" class="imagem">
+		<p class="paragrafo">Parágrafo provisorio...</p>
+		<span class="data">09/08/2022</span>
+	</div>
+	
+</div>
+
+<div id="recentes">
+
+</div>
---
+++ site_php/topo.php
@@ -0,0 +1,3 @@
+<img src="imagens/logo.png"  class="logo"/>
+<a href="form_login.php" class="links_top">Logar</a>
+<a href="cadastro.php" class="links_top">Cadastra-se</a>
